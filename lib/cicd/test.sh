90282749154f9fa662640e39b0dfd4a91a8b3fd1
Yusuf Ali <ali@yusuf.email>
2020-08-23 09:42:22 -0400

    fix: libtransfer fix

--- lib/cicd/test.sh
+++ lib/cicd/test.sh
@@ -2,15 +2,20 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+$DIR/pre.sh $1 $2
 cd $1
 
-REPO=$(basename $1)
-npm install
+if [ -f services.sh ]; then
+  ./services.sh
+fi
 
 # NPM Version Check
+REPO=$(basename $1)
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 PUBLISHED_VERSION=$(npm view chess_$REPO version)
 if [ "$PUBLISHED_VERSION" == "$PACKAGE_VERSION" ]; then
   echo "$REPO need to increment version!"
   exit 1; 
 fi
+
+npm test
