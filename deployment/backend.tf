48df9ef30a43d9a32763e21dfcbe8ae85d59e9e7
Yusuf Ali <ali@yusuf.email>
2020-10-15 10:11:43 -0400

    deploy: updated terraform

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,6 +1,26 @@
 terraform {
-  required_version = "0.12.25"
   
+  required_providers {
+    
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "2.11.0"
+    }
+    
+    docker = {
+      source = "terraform-providers/docker"
+    }
+    
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "1.12.0"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+
   backend "s3" {
     force_path_style = true
     bucket = "terraform"
@@ -10,16 +30,17 @@ terraform {
     skip_metadata_api_check = true
     skip_region_validation = true
   }
+
+  required_version = ">= 0.13"
 }
 
 provider "kubernetes" {
-  version = "1.12.0"
   load_config_file = "false"
 
   host = "https://${var.KUBERNETES_HOST}:${var.KUBERNETES_PORT}"
-  client_certificate = file("./secrets/kube/user.crt")
-  client_key = file("./secrets/kube/user.key")
-  cluster_ca_certificate = file("./secrets/kube/ca.crt")
+  client_certificate = file("./secrets/user.crt")
+  client_key = file("./secrets/user.key")
+  cluster_ca_certificate = file("./secrets/ca.crt")
 }
 
 resource "kubernetes_secret" "slack_url" {
@@ -31,13 +52,6 @@ resource "kubernetes_secret" "slack_url" {
   data = {
     SLACK_URL = var.slack_url
   }
-}
-
-provider "docker" {
-}
-
-provider "cloudflare" {
-  version = "~> 2"
 }
 
 # dns stuff
